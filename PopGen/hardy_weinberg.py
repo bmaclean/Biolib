fb85f4deb06a7361f6eb160330fae4ef52ea9bdc
brendanmaclean <brendan.maclean@alumni.ubc.ca>
2019-08-29 21:16:11 -0600

    Function to determine if population is in HWE.
    Add condition to get_allele_freq for if both phenotypic freqs are provided.

--- PopGen/hardy_weinberg.py
+++ PopGen/hardy_weinberg.py
@@ -7,10 +7,16 @@ import math
 def get_allele_freq(pp_freq=None, qq_freq=None):
     """
         Receives the phenotypic frequency of either pp or qq, and returns the allele
-        frequencies of p and q.
+        frequencies of alleles p and q.
     """
     if pp_freq is None and qq_freq is None:
         raise Exception
+    elif pp_freq is not None and qq_freq is not None:
+        # both phenotype frequencies provided
+        return {
+            'p': math.sqrt(pp_freq),
+            'q': math.sqrt(qq_freq)
+        }
     if qq_freq is None:
         # pp phenotype frequency provided
         return {
@@ -23,3 +29,20 @@ def get_allele_freq(pp_freq=None, qq_freq=None):
             'p': 1 - math.sqrt(qq_freq),
             'q': math.sqrt(qq_freq)
         }
+
+def in_hwe(gen_1, gen_2):
+    """
+        Given the allele frequenceis of two generations 1 and 2, returns True if that
+        population is in Hardy-Weinberg equilibrium between those generations.
+
+        gen_1 and gen_2 are of the format:
+        {
+            p: 0.7,
+            q: 0.3
+        }
+
+        Where p and q are alleles in the gene pool.
+
+        A `False` result from is_hwe suggests the given gene is not undergoing evolution.
+    """
+    return gen_1.p == gen_2.p and gen_1.q == gen_2.q
